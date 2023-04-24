8d68218c06a3eb5fcbae0449a6b8b491378b68c6
wong-1994 <seanwxj@tongji.edu.cn>
2023-04-24 13:00:28 +0800

    argument process

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,11 +1,56 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-  for (int i = 0; i < argc; i++) {
-    assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+int main(int argc, char *argv[])
+{
+  const char *help_message = "Usage: pstree [-pn]\n   or: pstree -V\n"
+                             "Display a tree of processes.\n"
+                             "  -p, --show-pids     show PIDs; implies -c\n"
+                             "  -n, --numeric-sort  sort output by PID\n"
+                             "  -V, --version       display version information\n";
+
+  struct option long_options[] = {
+      {"show-pids", no_argument, NULL, 'p'},
+      {"numeric-sort", no_argument, NULL, 'n'},
+      {"version", no_argument, NULL, 'V'},
+      {0, 0, 0, 0}};
+
+  char ch;
+  int option_index = 0;
+  char pFlag = 0;
+  char nFlag = 0;
+
+  while (-1 != (ch = getopt_long(argc, argv, "pnV", long_options, &option_index)))
+  {
+    switch (ch)
+    {
+    case 'p':
+      pFlag = 1;
+      break;
+    case 'n':
+      nFlag = 1;
+      break;
+    case 'V':
+      printf("pstree version: 2023\n");
+      break;
+    default:
+      printf("%s", help_message);
+      exit(0);
+    }
   }
-  assert(!argv[argc]);
+
+  if (pFlag)
+  {
+    printf("pid\n");
+  }
+
+  if (nFlag)
+  {
+    printf("numeric\n");
+  }
+
   return 0;
 }
