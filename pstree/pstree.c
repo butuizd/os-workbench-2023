12d9a6c3fdd72631b53098f8e299b4b44fe488ae
wong-1994 <seanwxj@tongji.edu.cn>
2023-04-24 16:48:05 +0800

    rearrange code

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -3,35 +3,44 @@
 #include <unistd.h>
 #include <getopt.h>
 #include <stdlib.h>
+#include <dirent.h>
 
-int main(int argc, char *argv[])
+#define true ((unsigned char)(1))
+#define false ((unsigned char)(0))
+
+/* 打印信息 */
+static const char *help_message = "Usage: pstree [-pn]\n   or: pstree -V\n"
+                                  "Display a tree of processes.\n"
+                                  "  -p, --show-pids     show PIDs; implies -c\n"
+                                  "  -n, --numeric-sort  sort output by PID\n"
+                                  "  -V, --version       display version information\n";
+
+/* 命令行参数结构 */
+static struct option long_options[] = {
+    {"show-pids", no_argument, NULL, 'p'},
+    {"numeric-sort", no_argument, NULL, 'n'},
+    {"version", no_argument, NULL, 'V'},
+    {0, 0, 0, 0}};
+
+/* 记录参数 */
+static unsigned char pFlag = false;
+static unsigned char nFlag = false;
+
+/* 命令行参数解析 */
+void parseArgs(int argc, char *argv[])
 {
-  const char *help_message = "Usage: pstree [-pn]\n   or: pstree -V\n"
-                             "Display a tree of processes.\n"
-                             "  -p, --show-pids     show PIDs; implies -c\n"
-                             "  -n, --numeric-sort  sort output by PID\n"
-                             "  -V, --version       display version information\n";
-
-  struct option long_options[] = {
-      {"show-pids", no_argument, NULL, 'p'},
-      {"numeric-sort", no_argument, NULL, 'n'},
-      {"version", no_argument, NULL, 'V'},
-      {0, 0, 0, 0}};
-
   char ch;
   int option_index = 0;
-  char pFlag = 0;
-  char nFlag = 0;
 
   while (-1 != (ch = getopt_long(argc, argv, "pnV", long_options, &option_index)))
   {
     switch (ch)
     {
     case 'p':
-      pFlag = 1;
+      pFlag = true;
       break;
     case 'n':
-      nFlag = 1;
+      nFlag = true;
       break;
     case 'V':
       printf("pstree version: 2023\n");
@@ -41,6 +50,11 @@ int main(int argc, char *argv[])
       exit(0);
     }
   }
+}
+
+int main(int argc, char *argv[])
+{
+  parseArgs(argc, argv);
 
   if (pFlag)
   {
